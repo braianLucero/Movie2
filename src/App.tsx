6dddb8b93f97f791b66c573f57e0bbf59f481afb
braian <brasanluc123@gmail.com>
2024-05-01 20:11:02 -0300

    class 143. installing Navigation between screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Navigation} from './presentation/navigation/Navigation';
 export const App = () => {
   return (
-    <View>
-      <Text>MovieApp</Text>
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 };
---
+++ src/presentation/navigation/Navigation.tsx
@@ -0,0 +1,19 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens/home/HomeScreen';
+import {DetailScreen} from '../screens/details/DetailsScreen';
+
+export type RootStackParams = {
+  Home: undefined;
+  Details: {numberId: number};
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailScreen} />
+    </Stack.Navigator>
+  );
+};
