d3357245b5c825562c1a33636e264a9054d6e355
braian <brasanluc123@gmail.com>
2024-05-02 18:15:16 -0300

    I repeat class 148. solving a Pattern Mapper problem

--- src/core/use-cases/movies/now-playing.use-case.ts
+++ src/core/use-cases/movies/now-playing.use-case.ts
@@ -1,5 +1,6 @@
 import {HttpAdapter} from '../../../config/adapters/http/http.adapter';
 import {NowPlayingResponse} from '../../../infrastructure/interface/movie-db.responses';
+import {MovieMapper} from '../../../infrastructure/mapper/movie.mapper';
 import type {Movie} from '../../entities/movie.entity';
 
 export const moviesNowPlayingUseCase = async (
@@ -8,7 +9,8 @@ export const moviesNowPlayingUseCase = async (
   try {
     const nowPlaying = await fetcher.get<NowPlayingResponse>('/now_playing');
     console.log({nowPlaying});
-    return [];
+
+    return nowPlaying.results.map(MovieMapper.fromMovieDBResultToEntity);
   } catch (error) {
     console.log(error);
     throw new Error('Error fetching movies - NowPlaying');
