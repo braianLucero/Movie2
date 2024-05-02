0b6728c09a27591112e52f526d9dd403b16d12a0
braian <brasanluc123@gmail.com>
2024-05-01 23:27:29 -0300

    class 147. creating a CustomHook - useMovies

---
+++ src/presentation/hooks/useMovies.tsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react';
+import type {Movie} from '../../core/entities/movie.entity';
+
+import * as UseCases from '../../core/use-cases';
+import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
+
+export const useMovies = () => {
+  const [isLoading, setIsLoading] = useState<Movie[]>([]);
+  const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    initialLoad();
+  }, []);
+
+  const initialLoad = async () => {
+    const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(
+      movieDBFetcher,
+    );
+  };
+
+  return {};
+};
