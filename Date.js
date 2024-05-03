e6a23a39e541e37f29eb065a27253fe923875948
braian <brasanluc123@gmail.com>
2024-05-02 23:24:22 -0300

    clas 162. creating Movie Information by ID

---
+++ Date.js
@@ -0,0 +1,41 @@
+{
+    "peliculas": [
+      {
+        "id": 1,
+        "titulo": "El Padrino",
+        "director": "Francis Ford Coppola",
+        "anio": 1972,
+        "genero": ["Drama", "Crimen"],
+        "sinopsis": "La historia se centra en la familia criminal Corleone y su líder, Don Vito Corleone.",
+        "puntuacion": 9.2
+      },
+      {
+        "id": 2,
+        "titulo": "El Señor de los Anillos: La Comunidad del Anillo",
+        "director": "Peter Jackson",
+        "anio": 2001,
+        "genero": ["Fantasía", "Aventura"],
+        "sinopsis": "Un joven hobbit llamado Frodo Bolsón hereda un anillo mágico y se embarca en una épica aventura para destruirlo.",
+        "puntuacion": 8.8
+      },
+      {
+        "id": 3,
+        "titulo": "Titanic",
+        "director": "James Cameron",
+        "anio": 1997,
+        "genero": ["Drama", "Romance"],
+        "sinopsis": "La historia de amor entre Jack y Rose que se desarrolla en el trasfondo del trágico hundimiento del Titanic.",
+        "puntuacion": 7.8
+      },
+      {
+        "id": 4,
+        "titulo": "Interestelar",
+        "director": "Christopher Nolan",
+        "anio": 2014,
+        "genero": ["Ciencia ficción", "Drama"],
+        "sinopsis": "Un grupo de exploradores espaciales viaja a través de un agujero de gusano en busca de un nuevo hogar para la humanidad.",
+        "puntuacion": 8.6
+      }
+    ]
+  }
+  
