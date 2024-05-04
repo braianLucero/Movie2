01c301dc223bf30ad5a385862a0d46a92b06fb36
braian <brasanluc123@gmail.com>
2024-05-03 21:41:07 -0300

    clas 162. finishing developing all the movie information by ID

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,7 +1,13 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Text, View} from 'react-native';
+import {RootStackParams} from '../../navigation/Navigation';
 
-export const DetailScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailScreen = ({route}: Props) => {
+  const {movieId} = route.params;
+  // const {movieId} = useRoute().params;
   return (
     <View>
       <Text>DetailScreen</Text>
