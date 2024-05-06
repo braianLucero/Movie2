5520642b611c955562e2c5d5d5b317d2cf443dab
braian <brasanluc123@gmail.com>
2024-05-05 22:57:53 -0300

    clas 173;showing the actors

--- src/presentation/components/movies/movie/MovieDetails.tsx
+++ src/presentation/components/movies/movie/MovieDetails.tsx
@@ -3,11 +3,12 @@ import {Text, View, FlatList} from 'react-native';
 import {FullMovie} from '../../../../core/entities/movie.entity';
 import {Formatter} from '../../../../config/helpers/formatter';
 import {Cast} from '../../../../core/entities/cast.etity';
+import {CastActor} from '../../cast/CastActor';
 interface Props {
   movie: FullMovie;
   cast: Cast[];
 }
-export const MovieDetails = ({movie, cast}: Props) => {
+export const MovieDetails = ({movie, cast = []}: Props) => {
   return (
     <>
       <View style={{marginHorizontal: 20}}>
@@ -28,7 +29,7 @@ export const MovieDetails = ({movie, cast}: Props) => {
         <Text style={{fontSize: 18}}>{Formatter.currency(movie.budget)}</Text>
       </View>
 
-      <View style={{marginTop: 10, marginBottom: 100}}>
+      <View style={{marginTop: 10, marginBottom: 50}}>
         <Text
           style={{
             fontSize: 23,
@@ -43,7 +44,7 @@ export const MovieDetails = ({movie, cast}: Props) => {
           keyExtractor={item => item.id.toString()}
           horizontal
           showsHorizontalScrollIndicator={false}
-          renderItem={({item}) => <Text>{item.name}</Text>}
+          renderItem={({item}) => <CastActor actor={item} />}
         />
       </View>
     </>
