b4cbc272e7b40dc8428942a807448ed918633b9c
braian <brasanluc123@gmail.com>
2024-05-05 23:22:02 -0300

    clas 174 ; creating environment variable

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,11 @@
+import {THE_MOVIE_DB_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '731f24a408a70c6bd83f9cb1cf12d64e',
+    // api_key: '731f24a408a70c6bd83f9cb1cf12d64e',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'es',
   },
 });
